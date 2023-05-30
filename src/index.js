202f1b4fb9847fcfd07093fcf0a7ba81b345554e
Gullit Moran <gullitmoran@gmail.com>
2023-05-30 15:17:32 -0500

    Actulizando URL del back

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store/store';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://back-calendar-8cf0e4c1h-morangullit.vercel.app/';
+axios.defaults.baseURL = 'https://back-calendar-ne6gcb85a-morangullit.vercel.app';
 
 ReactDOM.render(
   <React.StrictMode>
